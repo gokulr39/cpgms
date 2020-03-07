f4d95027fa1b89e971bc27ef9658164ae2b4cf5d
gokulr39 <61898981+gokulr39@users.noreply.github.com>
2020-03-07 12:29:29 +0530

    Add files via upload

---
+++ ac.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main()
+{
+ float r,A,C;
+ printf("enter radius r=");
+ scanf("%f",&r);
+ A=3.14*r*r;
+ printf("area of the circle=%f\n",A);
+ C=2*3.14*r;
+ printf("circumference of circle=%f\n",C);
+}
